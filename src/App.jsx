4874981cbbd27af8b1a27212efe259c47664ab5b
Matt Pahuta <mhpahuta@gmail.com>
2024-04-17 16:13:56 -0700

    Add hold dice functionality, styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,33 +4,63 @@ import { DICE_ARRAY_LENGTH, DIE_SIDE_COUNT } from "./constants";
 
 function App() {
   // ToDo: move to 'helpers' file
-  // ToDo: add 'range' function to a 'utils' file?
+
+    // move this to utils file
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * DIE_SIDE_COUNT),
+      isHeld: false,
+      id: Math.random()
+    }
+  }
+
+  // const generateNewDice = () => [...new Array(DICE_ARRAY_LENGTH)]
+  //   .map(() => {
+  //     return {
+  //       value: Math.ceil(Math.random() * DIE_SIDE_COUNT),
+  //       isHeld: false,
+  //       id: Math.random()
+  //     }
+  // });
 
   const generateNewDice = () => [...new Array(DICE_ARRAY_LENGTH)]
-    .map(() => {
-      return {
-        value: Math.ceil(Math.random() * DIE_SIDE_COUNT),
-        isHeld: false,
-        id: Math.random()
-      }
-    });
+    .map(() => generateNewDie());
 
   const [diceArray, setDiceArray] = React.useState(() => generateNewDice());
-  console.log(diceArray)
+  // console.log(diceArray)
   
-  // move this to inline onClick event for button?
+
+
   function rollDice() {
-    setDiceArray(() => generateNewDice())
+    // setDiceArray(generateNewDice())
+
+    setDiceArray(prevDice => prevDice.map(die => {
+      return die.isHeld ?
+        die :
+        generateNewDie()
+    }))
   }
+
+  function holdDice(id) {
+    console.log(id)
+    setDiceArray(prevDice => prevDice.map(die => {
+      return die.id === id ?
+        {...die, isHeld: !die.isHeld} : // flip isHeld for matching die id
+        die
+    }))
+  }
+
+  // isHeld ? 'die-face button die-button held-die' : 'die-face button die-button'
+  const diceElements = diceArray.map(({value, id, isHeld}) => (
+    <Die key={id} value={value} isHeld={isHeld} holdDice={() => holdDice(id)} />
+  ))
 
   return (
     <main className="main">
       {/* Future Header */}
       <h1 className="game-heading">Tenzies</h1>
       <div className='dice-grid'>
-        {diceArray.map(({value, id}) => (
-          <Die key={id} value={value} />
-        ))}
+        {diceElements}
       </div>
       <button className="button roll-button" onClick={rollDice}>Roll</button>
     </main>
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,8 +1,12 @@
 
-function Die({value}) {
+function Die({value, isHeld, holdDice}) {
+  // isHeld ? 'die-face button die-button held-die' : 'die-face button die-button'
+  const styles = {
+    backgroundColor: isHeld ? '#59E391' : '#FFFFFF',
+  }
 
   return (
-    <button className="die-face button die-button">
+    <button className='die-face button die-button held-die' style={styles} onClick={holdDice}>
       <span className="die-num">{value}</span>
     </button>
   )
